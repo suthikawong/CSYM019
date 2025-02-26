50bbddf46eb287c3b3a731a57fb0af9edb268f46
suthikawong <suthika.wsrd@gmail.com>
2025-02-26 10:00:56 +0000

    complete exercise 7

--- week5/script.js
+++ week5/script.js
@@ -1,16 +1,11 @@
 const onClickButton = () => {
   var element = document.getElementById('circle')
-  var circleOpacity = parseFloat(element.style.opacity)
-  setInterval(() => {
-    circleOpacity -= 0.01
-    element.style.opacity = circleOpacity
-  }, 10)
+  var positionLeft = element.offsetLeft
+  element.style.left = positionLeft - 10 + 'px'
 }
 
 const onLoad = () => {
-  var element1 = document.getElementById('circle')
-  element1.style.opacity = 1
-  element1.addEventListener('click', onClickButton)
+  document.addEventListener('keydown', onClickButton)
 }
 
 document.addEventListener('DOMContentLoaded', onLoad)

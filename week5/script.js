28bfb0815d8d9f6d6ef72f1ec9e9808d4ce76c0c
suthikawong <suthika.wsrd@gmail.com>
2025-02-26 09:41:24 +0000

    complete exercise 4

--- week5/script.js
+++ week5/script.js
@@ -1,11 +1,12 @@
 const onClickButton = () => {
   var element = document.getElementById('circle')
-  element.style.opacity = 0.5
+  element.style.opacity = 1
 }
 
 const onLoad = () => {
   var element1 = document.getElementById('circle')
   element1.addEventListener('click', onClickButton)
+  element1.style.opacity = 0.5
 }
 
 document.addEventListener('DOMContentLoaded', onLoad)
